8654364f4256d55ef5f1d310f384ec4b02795e88
mauro-balades <mauro.balades@tutanota.com>
2022-03-08 18:20:45 +0100

    added unary expressions

--- src/lib/emitter.ts
+++ src/lib/emitter.ts
@@ -27,6 +27,7 @@ import * as llvm from "llvm-node";
 
 import { LLVMGenerator } from "./generator";
 import { Scope } from "./enviroment/scopes";
+import { isValueType } from "./utils";
 
 class Emitter {
 
@@ -36,9 +37,64 @@ class Emitter {
         this.generator = generator;
     }
 
-    emitNode(node: ts.Node, scope: Scope) {
-
+    emitNode(node: ts.Node, scope: Scope): void {
+        switch (node.kind) {
+            case ts.SyntaxKind.ExpressionStatement:
+                this.emitExpressionStatement(node as ts.ExpressionStatement);
+        }
     }
+
+    /// GENERATION FUNCTIONS ///
+
+    emitExpressionStatement(statement: ts.ExpressionStatement): void {
+        this.emitLvalueExpression(statement.expression);
+    }
+
+    emitLvalueExpression(expression: ts.Expression): llvm.Value {
+        switch (expression.kind) {
+          case ts.SyntaxKind.PrefixUnaryExpression:
+            return this.emitPrefixUnaryExpression(expression as ts.PrefixUnaryExpression);
+          default:
+            throw Error(`Unhandled ts.Expression '${ts.SyntaxKind[expression.kind]}'`);
+        }
+    }
+
+    emitExpression(expression: ts.Expression): llvm.Value {
+        return this.convertToRvalue(this.emitLvalueExpression(expression));
+    }
+    
+    convertToRvalue(value: llvm.Value) {
+        if (value.type.isPointerTy() && isValueType(value.type.elementType)) {
+            return this.generator.builder.createLoad(value, value.name + ".load");
+        }
+        return value;
+    }
+
+    /// EXPRESIONS ///
+
+    emitPrefixUnaryExpression(expression: ts.PrefixUnaryExpression): llvm.Value {
+        const { operand } = expression;
+      
+        switch (expression.operator) {
+          case ts.SyntaxKind.PlusToken:
+            return this.emitExpression(operand);
+
+          case ts.SyntaxKind.MinusToken:
+            throw Error(`Unhandled ts.PrefixUnaryOperator operator '${ts.SyntaxKind[expression.operator]}'`);
+
+          case ts.SyntaxKind.PlusPlusToken:
+            throw Error(`Unhandled ts.PrefixUnaryOperator operator '${ts.SyntaxKind[expression.operator]}'`);
+
+          case ts.SyntaxKind.MinusMinusToken:
+            throw Error(`Unhandled ts.PrefixUnaryOperator operator '${ts.SyntaxKind[expression.operator]}'`);
+
+          case ts.SyntaxKind.TildeToken:
+            throw Error(`Unhandled ts.PrefixUnaryOperator operator '${ts.SyntaxKind[expression.operator]}'`);
+
+          case ts.SyntaxKind.ExclamationToken:
+            throw Error(`Unhandled ts.PrefixUnaryOperator operator '${ts.SyntaxKind[expression.operator]}'`);
+        }
+      }
 }
 
 export default Emitter;
